aa91b2556545fa453eab6443d632e69da2ff84fc
bbggkkk <bbggkkk@naver.com>
2024-05-21 15:27:09 +0900

    다국어 지원

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ function updateBodyClass() {
 updateBodyClass();
 // 모드 변경 시 클래스 업데이트
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', updateBodyClass);
-
 window.addEventListener('DOMContentLoaded', () => {
   document.querySelector(`md-radio[name="language"][value="${location.pathname.replace(/[^a-z]/g, '') || 'en'}"]`).checked = true;
   document.querySelector('#selectLanguageButton').addEventListener('click', (e) => {
