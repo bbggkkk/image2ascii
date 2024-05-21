f995417f19d2bf90ef5b7e1cac8e99b63872b8e6
bbggkkk <bbggkkk@naver.com>
2024-05-21 15:15:26 +0900

    다국어 지원

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ function updateBodyClass() {
   document.body.classList.toggle('dark', isDarkMode);
   document.body.classList.toggle('light', !isDarkMode);
 }
-
 // 초기 로드 시 클래스 설정
 updateBodyClass();
 // 모드 변경 시 클래스 업데이트
